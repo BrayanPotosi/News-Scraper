18d40eb63e994ffb9195e0f877c37477ba20246c
Brayan Potosi <bp990308@hotmail.com>
2021-08-01 14:32:51 -0500

    [ADD] parse_notice function

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
-import requests
 import lxml.html as html
+from datetime import date
+import requests
+import os
 
 SITE_URL = 'https://www.larepublica.co/'
 
@@ -12,15 +14,57 @@ XPATH_SUMMARY = '//div[@class="lead"]/p/text()'
 XPATH_BODY = '//div[@class="html-content"]/p[not(@class)]/text()'
 
 
+def get_title(link):
+
+    url = link.split('/')[-1]
+    title_list=url.split('-')[:-1]
+    title = " ".join(title_list)
+    return title
+
+
+def parse_notice(link, folder_title):
+    try:
+        response = requests.get(link)
+        if response.status_code == 200:
+            notice = response.content.decode('utf-8')
+            parsed = html.fromstring(notice)
+            try:
+                title = get_title(link)
+                summary = parsed.xpath(XPATH_SUMMARY)[0]
+                body = parsed.xpath(XPATH_BODY)
+
+            except IndexError:
+                return
+
+            with open(f'{folder_title}/{title}.txt', 'w', encoding='utf-8') as f:
+                f.write(f'{title}\n\n\n')
+                f.write(f'{summary}\n\n\n')
+                for p in body:
+                    f.write(f'{p}\n')
+
+        else:
+            raise ValueError(f'Error, status code {response.status_code}')
+    except ValueError as ve:
+        print(ve)
+
+
 def parse_home():
     try:
         response = requests.get(SITE_URL)
+
         if response.status_code == 200:
             home = response.content.decode('utf-8')
             parsed = html.fromstring(home)
-            link_news = parsed.xpath(XPATH_TOP_SECOND_NEWS)
-            for new in link_news:
-                print(new)
+            list_links = parsed.xpath(XPATH_SECOND_NEWS)
+            current_date = date.today().strftime('%d-%m-%Y')
+            folder_title = f'la_replublica_news_{current_date}'
+
+            if not os.path.isdir(folder_title):
+                os.mkdir(folder_title)
+
+            for link in list_links:
+                parse_notice(link, folder_title)
+
         else:
             raise ValueError(f'Error, status code: {response.status_code}')
     except ValueError as ve:
